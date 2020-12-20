8b11e92b4ca87433f01abfec1b484d695acdca86
ChrisGriffin0815 <66988781+ChrisGriffin0815@users.noreply.github.com>
2020-12-19 22:02:36 -0700

    changed db setup

---
+++ amazen-server/Models/Product.cs
@@ -0,0 +1,20 @@
+namespace amazen_server.Models
+{
+    public class Product
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Size { get; set; }
+        public decimal Price { get; set; }
+        public string creatorId { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+        public bool isAvailable { get; set; }
+    }
+
+    public class WishlistProductViewModel : Product
+    {
+        public int WishlistProductId { get; set; }
+    }
+}
---
+++ amazen-server/Services/setup.sql
@@ -0,0 +1,46 @@
+
+-- CREATE TABLE colors (
+--   id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+--   color VARCHAR(255),
+--   image VARCHAR(255)
+-- -- )
+-- CREATE TABLE products (
+--   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   title VARCHAR(255) NOT NULL,
+--   description VARCHAR(255) NOT NULL,
+--   image VARCHAR(255) NOT NULL,
+--   size VARCHAR(255),
+--   price DECIMAL (4,2) NOT NULL,
+--   creatorId VARCHAR(255) NOT NULL,
+--   likes int,
+--   dislikes int,
+--   isAvailable TINYINT NOT NULL,
+--   FOREIGN KEY (creatorId) REFERENCES profiles (id) ON DELETE CASCADE
+-- )
+  -- ALTER TABLE products
+  -- ADD COLUMN colorId int NOT NULL
+  -- drop TABLE products
+
+-- CREATE TABLE wishlists (
+--   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   title VARCHAR(255),
+--   creatorId int NOT NULL
+-- )
+
+-- drop table colors
+
+-- create table wishlistProducts (
+--   id int not null AUTO_INCREMENT PRIMARY KEY,
+--   wishlistId int,
+--   productId int,
+--   creatorId VARCHAR(255) not null,
+--   FOREIGN KEY (wishlistId)
+--   REFERENCES wishlists (id)
+--   ON DELETE CASCADE,
+--   FOREIGN KEY (productId)
+--   REFERENCES products (id)
+--   ON DELETE CASCADE,
+--   FOREIGN KEY (creatorId)
+--   REFERENCES profiles (id)
+--   ON DELETE CASCADE
+-- )
